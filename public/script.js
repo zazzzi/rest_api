76432c01621ff56e0822edc88e319f73ae74c3b7
Sebastian Zazzi <sebastian@zazzi.se>
2021-04-12 19:35:49 +0200

    big commit.... front end now working

---
+++ public/script.js
@@ -0,0 +1,252 @@
+window.addEventListener("load", initSite());
+
+//Initial start-up
+function initSite() {
+  console.log("Initial start-up");
+  getAllCharacter();
+}
+
+// Get all characters and render them to the DOM
+async function getAllCharacter() {
+  const allChars = await fetchRequest("/api/characters/", "GET");
+  console.log("all characters", allChars);
+
+  //render all chars
+  for (let i = 0; i < allChars.length; i++) {
+    renderCharacters(allChars[i]);
+  }
+  renderAddNewCard();
+}
+
+// Function for render and layout
+function renderCharacters(characters) {
+  const wrapper = document.getElementById("gridWrapper");
+  const cardWrapper = document.createElement("li");
+  cardWrapper.classList.add("cardsItem");
+  const card = document.createElement("div");
+  card.classList.add("card");
+  const cardContent = document.createElement("div");
+  cardContent.classList.add("cardContent");
+
+  //image
+  const imgTag = document.createElement("img");
+  imgTag.src = characters.img;
+  imgTag.classList.add("cardImage");
+
+  //title / name
+  const nameTag = document.createElement("h2");
+  nameTag.innerHTML = characters.name;
+  nameTag.classList.add("cardTitle");
+
+  //rank
+  const rankTag = document.createElement("p");
+  rankTag.innerHTML = characters.rank;
+  rankTag.classList.add("cardText");
+
+  //btn
+  const btn = document.createElement("button");
+  btn.innerHTML = "Edit Character";
+  btn.classList.add("editBtn");
+  btn.addEventListener("click", () => {
+    setEditModalContent(characters);
+    handleModal();
+  });
+
+  card.append(imgTag);
+  cardContent.append(nameTag);
+  cardContent.append(rankTag);
+  cardContent.append(btn);
+  card.append(cardContent);
+
+  cardWrapper.append(card);
+  wrapper.append(cardWrapper);
+}
+
+//modal handler
+function handleModal() {
+  const modal = document.getElementById("myModal");
+  const modalWrapper = document.getElementById("modalWrapper");
+
+  modal.style.display = "block";
+
+  const span = document.getElementsByClassName("close")[0];
+  span.addEventListener("click", () => {
+    modal.style.display = "none";
+    modalWrapper.innerHTML = "";
+  });
+  window.addEventListener("click", (event) => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      modalWrapper.innerHTML = "";
+    }
+  });
+}
+
+/**
+ * Sets the modal to edit existing character in the DB
+ * @param {string} character which character to update
+ */
+function setEditModalContent(character) {
+  const modalWrapper = document.getElementById("modalWrapper");
+  const modalForm = document.createElement("form");
+  modalForm.classList.add("modalWrapperForm");
+
+  //name input and lable
+  const nameLable = document.createElement("h4");
+  nameLable.innerHTML = "Name of character:";
+  const name = document.createElement("input");
+  name.value = character.name;
+
+  //rank input and lable
+  const rankLable = document.createElement("h4");
+  rankLable.innerHTML = "Rank of character:";
+  const rank = document.createElement("input");
+  rank.value = character.rank;
+
+  //image input and lable
+  const imgLable = document.createElement("h4");
+  imgLable.innerHTML = "Image of character (Link):";
+  const img = document.createElement("input");
+  img.value = character.img;
+
+  const btnDiv = document.createElement("div");
+  // save button
+  const saveBtn = document.createElement("button");
+  saveBtn.classList.add("btn", "saveBtn");
+  saveBtn.innerHTML = "Save";
+  saveBtn.addEventListener("click", () => {
+    updateCharacter(character.id, name.value, rank.value, img.value);
+    window.location.reload();
+  });
+
+  // delete button
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("btn", "deleteBtn");
+  deleteBtn.innerHTML = "Delete";
+  deleteBtn.addEventListener("click", () => {
+    deleteCharacter(character.id);
+    window.location.reload();
+  });
+
+  modalForm.append(nameLable);
+  modalForm.append(name);
+  modalForm.append(rankLable);
+  modalForm.append(rank);
+  modalForm.append(imgLable);
+  modalForm.append(img);
+  modalWrapper.append(modalForm);
+  modalWrapper.append(btnDiv)
+  btnDiv.append(saveBtn);
+  btnDiv.append(deleteBtn);
+}
+
+/**
+ * Sends update request to server, with information from EditModal
+ * @param {string} id id of the character to update
+ * @param {string} name updated name
+ * @param {string} rank updated rank
+ * @param {string} img updated image
+ */
+function updateCharacter(id, name, rank, img) {
+  const body = {
+    name: name,
+    rank: rank,
+    img: img,
+  };
+  fetchRequest(`/api/characters/${id}`, "PUT", body);
+}
+
+/**
+ *
+ * @param {string} id id of character to delete
+ */
+function deleteCharacter(id) {
+  fetchRequest(`/api/characters/${id}`, "DELETE");
+}
+
+// renders the add new character icon to DOM
+function renderAddNewCard() {
+  const wrapper = document.getElementById("gridWrapper");
+  const cardWrapper = document.createElement("li");
+  cardWrapper.classList.add("newCharacter");
+
+  const imgTag = document.createElement("img");
+  imgTag.src = "./assets/addNew.png";
+  imgTag.classList.add("addNewImg");
+  imgTag.addEventListener("click", () => {
+    handleModal();
+    setNewCharModalContent();
+  });
+
+  cardWrapper.append(imgTag);
+  wrapper.append(cardWrapper);
+}
+
+//sets the modal to create a new character and push to db on Save
+function setNewCharModalContent() {
+  const modalWrapper = document.getElementById("modalWrapper");
+  const modalForm = document.createElement("form");
+  modalForm.classList.add("modalWrapperForm");
+
+  const nameLable = document.createElement("h4");
+  nameLable.innerHTML = "Name of character";
+  const name = document.createElement("input");
+  const rankLable = document.createElement("h4");
+  rankLable.innerHTML = "Rank of character";
+  const rank = document.createElement("input");
+  const imgLable = document.createElement("h4");
+  imgLable.innerHTML = "Image of character (Link):";
+  const img = document.createElement("input");
+
+  const saveBtn = document.createElement("button");
+  saveBtn.classList.add("btn", "saveBtn");
+  saveBtn.innerHTML = "Save";
+  saveBtn.addEventListener("click", () => {
+    saveNewCharacter(name.value, rank.value, img.value);
+    window.location.reload();
+  });
+
+  modalForm.append(nameLable);
+  modalForm.append(name);
+  modalForm.append(rankLable);
+  modalForm.append(rank);
+  modalForm.append(imgLable);
+  modalForm.append(img);
+  modalWrapper.append(modalForm);
+  modalWrapper.append(saveBtn);
+}
+
+/**
+ *
+ * @param {string} name name of new char
+ * @param {string} rank rank of new char
+ * @param {string} img image of new char
+ */
+async function saveNewCharacter(name, rank, img) {
+  const body = {
+    id: null,
+    name: name,
+    rank: rank,
+    img: img,
+  };
+
+  await fetchRequest("/api/characters", "POST", body);
+}
+
+/**
+ * Main fetch function, can be used for full crum
+ * @param {string} url endpoint url
+ * @param {string} method method for server call (GET POST PUT DELETE)
+ * @param {} body body to send as req
+ * @returns returns respons from server
+ */
+async function fetchRequest(url, method, body) {
+  const response = await fetch(url, {
+    method: method,
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const content = await response.json();
+  return content;
+}
--- server.js
+++ server.js
@@ -5,26 +5,27 @@ const port = 6969;
 
 //fs
 const fs = require("fs");
-const data = fs.readFileSync("data.json");
-const characters = JSON.parse(data);
-
-let starTrekChars = characters;
 
 app.use(express.json());
 app.use(express.static("./public"));
 
-//send placeholder array
+//get all characters
 app.get("/api/characters", (req, res) => {
-  res.send(starTrekChars);
+  const data = fs.readFileSync("data.json");
+  const characters = JSON.parse(data);
+
+  res.send(characters);
 });
 
 //post new object
 app.post("/api/characters", (req, res) => {
   const incomingObject = req.body;
   incomingObject.id = uniqId.time();
+  const data = fs.readFileSync("data.json");
+  const characters = JSON.parse(data);
 
-  starTrekChars.push(incomingObject);
-  const dataToWrite = JSON.stringify(starTrekChars, null, 2);
+  characters.push(incomingObject);
+  const dataToWrite = JSON.stringify(characters, null, 2);
   fs.writeFile("data.json", dataToWrite, () => console.log("Written to DB"));
 
   res.status(201).json(incomingObject);
@@ -32,23 +33,38 @@ app.post("/api/characters", (req, res) => {
 
 //update existing character
 app.put("/api/characters/:id", (req, res) => {
-  const indexOfChar = starTrekChars.findIndex((c) => c.id === req.params.id);
+  const data = fs.readFileSync("data.json");
+  const characters = JSON.parse(data);
+  const indexOfChar = characters.findIndex((c) => c.id === req.params.id);
   console.log(indexOfChar);
 
-  starTrekChars[indexOfChar].name = req.body.name;
-  starTrekChars[indexOfChar].rank = req.body.rank;
-  starTrekChars[indexOfChar].img = req.body.img;
+  const updatedCharacter = req.body;
+  characters[indexOfChar].name = updatedCharacter.name;
+  characters[indexOfChar].rank = updatedCharacter.rank;
+  characters[indexOfChar].img = updatedCharacter.img;
+  fs.writeFile("data.json", JSON.stringify(characters, null, 2), () =>
+    console.log("Written to DB")
+  );
+
+  res.status(200);
 });
 
 //delete character from ID
 app.delete("/api/characters/:id", (req, res) => {
+  const data = fs.readFileSync("data.json");
+  let characters = JSON.parse(data);
   console.log(req.params.id);
-  starTrekChars = starTrekChars.filter(({ id }) => id !== req.params.id);
+  characters = characters.filter(({ id }) => id !== req.params.id);
+  fs.writeFile("data.json", JSON.stringify(characters, null, 2), () =>
+    console.log("Written to DB")
+  );
 });
 
 // Get character from ID
 app.get("/api/characters/:id", (req, res) => {
-  const character = starTrekChars.find((c) => c.id === req.params.id);
+  const data = fs.readFileSync("data.json");
+  const characters = JSON.parse(data);
+  const character = characters.find((c) => c.id === req.params.id);
   !character
     ? res.status(404).send("Sorry, this person doenst exist")
     : res.send(character);
